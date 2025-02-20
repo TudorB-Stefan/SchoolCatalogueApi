4a84564e0c0ed9c3c8e0b3e49b4cb200a6638520
TudorB-Stefan <tudor.boana@gmail.com>
2025-02-20 15:00:56 +0200

    Test

---
+++ SchoolCatalogueApi/Migrations/20250220114240_DbRules.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolCatalogueApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class DbRules : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Year",
+                table: "SClasses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Serie",
+                table: "SClasses",
+                type: "nvarchar(1)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Year",
+                table: "SClasses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Serie",
+                table: "SClasses",
+                type: "nvarchar(1)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(1)");
+        }
+    }
+}
